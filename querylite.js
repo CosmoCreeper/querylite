aef1a46c45cebf92f5c6a294b7ba214c5bfa2370
CosmoCreeper <lathamsolomon12@gmail.com>
2024-12-11 18:44:01 -0500

    Update querylite.js

--- querylite.js
+++ querylite.js
@@ -1,20 +1,20 @@
 // Why choose us?
-var $whyUs = () => console.log("We shorten your everyday JS functions and add a little spice to your casual work life.");
-var $whyNotJquery = () => console.log("JQuery is a semi-outdated framework that does most of the stuff we do. Although it was great in its time, JS can do most of what it does nowadays. Ours just spruces it up.");
+const $whyUs = () => console.log("We shorten your everyday JS functions and add a little spice to your casual work life.");
+const $whyNotJquery = () => console.log("JQuery is a semi-outdated framework that does most of the stuff we do. Although it was great in its time, JS can do most of what it does nowadays. Ours just spruces it up.");
 // DOM manipulation.
-var $ = (id) => document.querySelector(id);
-var $all = (id) => document.querySelectorAll(id);
-var $id = (id) => document.getElementById(id);
-var $class = (cl) => document.getElementsByClassName(cl);
-var $tag = (tag) => document.getElementsByTagName(tag);
+const $ = (id) => document.querySelector(id);
+const $all = (id) => document.querySelectorAll(id);
+const $id = (id) => document.getElementById(id);
+const $class = (cl) => document.getElementsByClassName(cl);
+const $tag = (tag) => document.getElementsByTagName(tag);
 // Special parameters of DOM elements.
-var $style = (id) => document.querySelector(id).style;
-var $text = (id) => document.querySelector(id).textContent;
-var $html = (id) => document.querySelector(id).innerHTML;
-var $for = (id, cb) => document.querySelectorAll(id).forEach(cb);
+const $style = (id) => document.querySelector(id).style;
+const $text = (id) => document.querySelector(id).textContent;
+const $html = (id) => document.querySelector(id).innerHTML;
+const $for = (id, cb) => document.querySelectorAll(id).forEach(cb);
 // Console functions.
-var $log = (msg) => console.log(msg);
-var $error = (err) => console.error(err);
-var $debug = (deb) => console.debug(deb);
-var $info = (info) => console.info(info);
-var $warn = (warning) => console.warn(warning);
+const $log = (msg) => console.log(msg);
+const $error = (err) => console.error(err);
+const $debug = (deb) => console.debug(deb);
+const $info = (info) => console.info(info);
+const $warn = (warning) => console.warn(warning);
